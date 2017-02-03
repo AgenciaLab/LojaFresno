f4f03fe3e75908c95edb3e684f611d76ca1ad373
Alesson <ealesson@gmail.com>
2017-02-03 15:36:41 -0200

    aplicando thema js

--- LojaFresno/js/modules/theme.js
+++ LojaFresno/js/modules/theme.js
@@ -1,7 +1,7 @@
 (function($){
 
     // cart products
-    $.getJSON( "/mvc/store/cart/count?loja=249009", function( data ) {
+    $.getJSON( "/mvc/store/cart/count?loja=365499", function( data ) {
         var cartItem,
             cartItemHtml = '',
             totalItemsCart = data.cart.Products.length;
@@ -11,27 +11,27 @@
         }
 
         $('.cart .list ul').prepend(cartItemHtml);
-    }); 
+    });
 
     // plugins init
     if( $.fn.jcarousel ){
         $(".product-list > ul").jcarousel({ scroll: 4 });
         $(".brands > ul").jcarousel();
     }
-    
+
     var lazyActive;
     $("img.lazy").lazyload({
         event: "sporty"
     });
-    
+
     $('.product-list').find('.jcarousel-prev').click(function(){
         callLazy($(this).parents('.product-list'));
     });
-    
+
     $('.product-list').find('.jcarousel-next').click(function() {
         callLazy($(this).parents('.product-list'));
     });
-    
+
     $('.texto_variacao > h2').val("Tamanho:");
 
     function callLazy($showcase) {
@@ -46,7 +46,7 @@
         }
 
     }
-    
+
     // bootstrap classes for closed pages
     var page = $('html').attr('class');
     $('.relacionado').addClass('col-md-3 col-xs-12');
@@ -55,9 +55,9 @@
     $('#span_erro_carrinho').addClass('alert alert-info');
     $('.blocoSucesso').addClass('alert alert-success');
     $('.produtos-relacionados').parents('#ProdBlock').addClass('wrap-related');
-    
+
     switch (page) {
-        
+
         case 'page-product':
             $('#esquerda').addClass('col-md-8 col-xs-12');
             $('#direita').addClass('col-md-4 col-xs-12');
@@ -68,93 +68,93 @@
             $('#descricao').parents('#ProdBlock').addClass('wrap-tabs');
             $('.textarea').addClass('text');
             break;
-        
+
         case 'page-login':
             $('.caixa-login').addClass('col-md-6 col-xs-12');
             $('.caixa-cadastro').addClass('col-md-6 col-xs-12');
-            
+
             break;
-            
+
         case 'page-register':
             $('#CadastroAbas ul').addClass('nav nav-tabs');
             $('.botao-prosseguir-cadastro').addClass('btn btn-primary');
             break;
-            
+
         case 'page-checkout_cart':
             $('#tabela_carrinho').addClass('table table-hover table-bordered');
             $('.caixa-forma-frete').addClass('table table-hover table-bordered');
             $('.qntd').addClass('text');
             $('.caixa-cupom').parents('tr').addClass('wrap-cupom');
             $('#tab').parents('tr').addClass('wrap-shipping-forms');
-            
+
             // Carrinho adicionando linha sub-total
             var subTotalAux = $('#tabela_carrinho .valores_carrinho'),
             i = subTotalAux.size(),
             sumSubTotal = 0;
-            
+
             for(i = 0; i < subTotalAux.size(); i++){
                 if(i % 2 == 1){
                     sumSubTotal += parseFloat(subTotalAux.eq(i).text().replace("R$","").replace(",", "."));
                 }
             }
-    
+
             sumSubTotal = sumSubTotal.toFixed(2);
             sumSubTotal = sumSubTotal.toString().replace(".", ",");
             $('#tabela_carrinho').find('tbody').append('<tr class="sub-total"><td colspan="4">Sub-total:</td><td colspan="2" class="sub-total-value">R$ '+sumSubTotal+'</td></tr>');
 
             break;
-            
+
         case 'page-checkout_payment':
             $('table').addClass('table table-hover table-bordered');
             $('.caixa-forma-a_vista li').addClass('col-md-1');
             $('.caixa-forma-parcelado li').addClass('col-md-1');
             $('#obs').addClass('text');
             break;
-        
-            
+
+
         case 'page-implantacao':
             $('#implantation-news-nome').addClass('text');
             $('#implantation-news-email').addClass('text');
             $('.implantation button').addClass('btn btn-primary');
             break;
-            
+
         case 'page-central_cliente':
             $('.icoFilho').addClass('col-md-2');
             break;
-            
+
         case 'page-central_detalhe_pedido':
             $('table').addClass('table table-hover table-bordered');
             break;
-            
+
         case 'page-central_anteriores':
             $('table').addClass('table table-hover table-bordered');
             break;
-            
+
         case 'page-central_confirmar_pagamento':
             $('table').addClass('table table-hover table-bordered');
             break;
-            
+
         case 'page-central_rastrear':
             $('table').addClass('table table-hover table-bordered');
             break;
-            
+
         case 'page-central_troca':
             $('table').addClass('table table-hover table-bordered');
             break;
-            
+
         case 'page-central_lista_espera':
             $('table').addClass('table table-hover table-bordered');
             break;
-        
+
         default:
             // code
     }
-    
+
     // increase image of related products
 	var imgSrc,
 		i = 0,
 		total = 0;
-		
+
 	var $relacionado = $('.relacionado-imagem').find('img');
 	total = $relacionado.size();
 
@@ -163,13 +163,13 @@
 		imgSrc = $relacionado.eq(i).attr('src').replace('/90_','/');
 		$relacionado.eq(i).attr('src', imgSrc);
 	}
-	
+
     // 	back top button
     $('.backtop').click(function(){
         $('html, body').animate({scrollTop : 0},800);
         return false;
     });
-    
+
     // cart products
     $.getJSON( "/mvc/store/cart/count?loja=352023", function( data ) {
         var cartItem,
@@ -181,28 +181,28 @@
         }
 
         $('.cart .list ul').prepend(cartItemHtml);
-    }); 
-    
+    });
+
     setTimeout(function() {
       $('.page-product .varTit').append('<a href="http://www.dizup.com.br/sac_bandup/medidas/medidas.html" class="table-meansure" target="_blank">Tabela de medidas</a>');
     }, 3100);
-	
-	$('.page-product .lista-radios-input input[type=radio]').click(function(){ 
+
+	$('.page-product .lista-radios-input input[type=radio]').click(function(){
 	    var linkTabela = '<a href="http://www.dizup.com.br/sac_bandup/medidas/medidas.html" class="table-meansure" target="_blank">Tabela de medidas</a>';
-        setTimeout(function() { 
+        setTimeout(function() {
             $('.table-meansure').remove();
             $('.page-product .varTit').append(linkTabela);
         }, 2700);
-        clearTimeout(); 
-	});  
-    
+        clearTimeout();
+	});
+
     // esconde miniaturas do produto caso haja apenas uma
 	if ($('#foto_a li').length == 1) {
 		$('#foto_a').hide();
 	}
-	
+
 	$('.show-categories').click(function(){
-        $('.main-menu').toggle(); 
+        $('.main-menu').toggle();
 	});
-    
+
 })(jQuery);
