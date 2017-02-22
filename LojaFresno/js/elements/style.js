7c08e8ed9aa06a895c189f4ebf21ea3fe1c4c3f5
Alesson <ealesson@gmail.com>
2017-02-22 09:30:04 -0300

    last changeds before D day

--- LojaFresno/js/elements/style.js
+++ LojaFresno/js/elements/style.js
@@ -1,15 +1,4 @@
-var lastScrollTop = 0;
-            
-jQuery(window).scroll(function(event){
-    var st = jQuery(this).scrollTop();
-    if (st > lastScrollTop){
-        jQuery("#produtoAreaPesquisa").fadeOut(300);
-    } else {
-        jQuery("#produtoAreaPesquisa").fadeIn(300);
-    }
-    lastScrollTop = st;
-});
-            
+           
 jQuery(document).ready(function() {
     jQuery("#owl-demo").owlCarousel({
       singleItem:true,
