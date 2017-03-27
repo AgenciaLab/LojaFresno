45c4cb64862f7c1c2c53c592c531cac1f4c9c76b
Alesson <ealesson@gmail.com>
2017-03-27 11:56:05 -0300

    corrigido erros no layout mobile

--- LojaFresno/js/modules/theme.js
+++ LojaFresno/js/modules/theme.js
@@ -104,7 +104,7 @@
             $('.bt-cupom img').attr("src", 'https://images.tcdn.com.br/img/img_prod/203321/1473884780_btn-calcular-frete-carrinho.png');
 
             $('.bt-continuar-comprando a').append('<span class="botao-commerce-img">Continuar comprando</span>');
-            $('.page-checkout_cart .AvancarTopo+.bt-avancar, .page-checkout_cart .bt-continuar-comprando+.bt-avancar').append('<font class="btn-avancar"> AvanÃ§ar </font>');
+            $('.page-checkout_cart .AvancarTopo+.bt-avancar, .page-checkout_cart .bt-continuar-comprando+.bt-avancar').append('<font class="btn-avancar"> Avançar </font>');
 
             sumSubTotal = sumSubTotal.toFixed(2);
             sumSubTotal = sumSubTotal.toString().replace(".", ",");
