0c227ad314ffe4b77bb651c8fb5ffb5ea2750b7c
Alesson <ealesson@gmail.com>
2017-03-31 11:29:07 -0300

    correcao linha produto, link mobile e exclusao oficial

--- LojaOficial/js/elements/style.js
+++
@@ -1,9 +0,0 @@
-           
-jQuery(document).ready(function() {
-    jQuery("#owl-demo").owlCarousel({
-      singleItem:true,
-      loop:true,
-      autoPlay : 5000,
-      stopOnHover : false
-    });
-  });
